9d766f2900a1034e2eb8f2423ba76af8ac196612
github username <github email address>
2018-01-29 14:56:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (collection, name) {
+  return collection.filter(function (dName) {
+    return dName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (collection, letters) {
+  return collection.filter(function (nLetters) {
+    return nLetters.startsWith(letters);
+  });
+}
+
+function matchName (collection, name) {
+  return collection.filter(function (driver) {
+    return driver.name === name;
+  });
+}
